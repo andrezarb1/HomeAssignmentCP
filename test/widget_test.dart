5737c8c5fca1e8ecd1bd385946d14b6b7fbf9226
ZarbA <ZarbA@LAPTOP-6PPSM2FL>
2026-01-25 16:12:07 +0100

    Fix widget tests and hive setup for ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,26 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_test/hive_test.dart';
 
 import 'package:snaptask_reminder/main.dart';
+import 'package:snaptask_reminder/models/task.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  setUp(() async {
+    await setUpTestHive();
+    Hive.registerAdapter(TaskAdapter());
+    await Hive.openBox<Task>('tasks');
+  });
+
+  tearDown(() async {
+    await tearDownTestHive();
+  });
+
+  testWidgets('Shows empty task list state', (WidgetTester tester) async {
     await tester.pumpWidget(const SnapTaskApp());
+    await tester.pumpAndSettle();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('SnapTask Reminder'), findsOneWidget);
+    expect(find.textContaining('No tasks yet'), findsOneWidget);
   });
 }
